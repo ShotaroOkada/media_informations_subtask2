114d9031bd1a918923ed19e6eb80c1cec766abf0
ShotaroOkada <b1016117@gmail.com>
2020-10-07 14:29:07 +0900

    complete subtask1

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from PIL import Image
 import numpy as np
 
 img = Image.open('terasawa.jpg')
-imgArray = np.array(img)
-imgArray[100:300, 100:300, :] = 0
-img = Image.fromarray(imgArray)
+img = img.convert("L")
+img = img.resize((256, 256))
+img = np.array(img)
+w, h = img.shape
+
+for y in range(h):
+    for x in range(w):
+        if x > h/2:
+            img[y, x] = 255 - img[y, x]
+        else:
+            img[y, x] = img[y, x]/2
+
+img = Image.fromarray(img)
 img.show()
