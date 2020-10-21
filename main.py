c83527a832f9516d93256069666b95ddc264cc3f
ShotaroOkada <b1016117@gmail.com>
2020-10-21 16:24:44 +0900

    サンプルコードを実行するところまで

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-import cv2
-import matplotlib.pyplot as plt
+import os
+import cv2 as cv
 import numpy as np
 
+os.chdir('test')
+image_list = os.listdir('./')
+chists = np.zeros((3000, 64), np.float32)
+fnames = []
 
-def identity(image):
-    h, w = image.shape[:2]
-    src = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0]], np.float32)
-    affine = cv2.getAffineTransform(src, src)
-    return cv2.warpAffine(image, affine, (w, h))
+for i, fname in enumerate(image_list):
+    fnames.append(fname)
+    hist = np.zeros((4, 4, 4), np.float32)
+    print(str(i)+':'+fname)
+    img = cv.imread(fname)
+    Height = img.shape[0]
+    Width = img.shape[1]
+    for y in range(Height):
+        for x in range(Width):
+            b = img[y, x, 0]//64
+            g = img[y, x, 1]//64
+            r = img[y, x, 2]//64
+            hist[r, b, g] += 1
 
+    hist = hist.reshape((1, 64))
+    hist = hist/(Height*Width)
+    chists[i, :] = hist[0, :]
 
-def rotate(image, angle):
-    h, w = image.shape[:2]
-    affine = cv2.getRotationMatrix2D((0, 0), angle, 1.0)
-    return cv2.warpAffine(image, affine, (w, h))
+query_id = 373
+print('query: '+fnames[query_id])
+dists = np.ones((3000), np.float32)*999
 
+for i in range(3000):
+    v = chists[query_id]-chists[i]
+    dists[i] = np.linalg.norm(v, ord=1)
+for r in range(6):
+    minval = np.min(dists)
+    minidx = np.argmin(dists)
+    #print('rank '+str(r)+': '+str(minidx)+' '+fnames[minidx]+', '+str(minval))
+    print('rank '+str(r)+': '+fnames[minidx]+', '+str(minval))
+    dists[minidx] = 999
 
-if __name__ == "__main__":
-    image = cv2.imread("terasawa.jpg")[:, :, ::-1]
-    converted = rotate(image, 20)
-    plt.imshow(converted)
-    plt.title("Identity")
-    plt.show()
+cv.waitKey(0)
+cv.destroyAllWindows()
